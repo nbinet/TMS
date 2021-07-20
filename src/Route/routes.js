d4191ae2a943dc334c3cd391a614f9ca2ea46e82
RomainDanizel <romain.danizel@hetic.net>
2021-07-20 16:24:36 +0200

    routes up

--- src/Route/routes.js
+++ src/Route/routes.js
@@ -1,69 +1,65 @@
-// import React from "react";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Link
-// } from "react-router-dom";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { LoginPage } from "../scenes/Login/Login";
+import { MapBuilding } from "../scenes/MapBuilding/MapBuilding";
 
-// const Route = ({ component: Component, roles, path }) => {
-//     roles = roles || [];
-//     return (
-//         <Route
-//             path={path}
-//             exact={true}
-//             render={(props) => 
-//                 hasRoles(roles) ? (
-//                     <Component {...props} />
-//                 ) : (
-//                     isAuth() ? (
-//                         <Unauthorized />
-//                     ) : (
-//                         <Redirect to="/login" />
-//                     )
-//                 )
-//             }
-//         />
-//     );
-// }
-type Route = {
-    name: string,
-    path: string,
-    component?: React.AbstractComponent<any>,
-    roles?: string[],
-    routes?: Route[],
- };
+// Some folks find value in a centralized route config.
+// A route config is just data. React is great at mapping
+// data into components, and <Route> is a component.
 
+// Our route config is just an array of logical "routes"
+// with `path` and `component` props, ordered the same
+// way you'd do inside a `<Switch>`.
 const routes = [
-    {
-        'name': 'login',
-        'path': '/login',
-        'component': Login,
-    },
-    {
-        'name': 'index',
-        'path': '/',
-        'component': Index,
-        'roles': ['ROLE_USER'],
-    },
- ];
-const compile = (parentRoute: Route, subRoutes: Route[]): Route[] => {
-    return subRoutes.flatMap(subRoute => {
-        const newRoute: Route = {
-            'name': subRoute.name,
-            'path': parentRoute.path + subRoute.path,
-            'component': subRoute.component,
-            'roles': (parentRoute.roles || []).concat((subRoute.roles || [])),
-        };
-        return (subRoute.routes) ? [...compile(newRoute, subRoute.routes)] : newRoute;
-    });
- }
- 
- export const getRoutes = () => {
-    const parentRoute = {
-        'name': '',
-        'path': '',
-    };
-    const flatRoutes = compile(parentRoute, routes);
-    return flatRoutes;
- }
+  {
+    path: "/",
+    component: LoginPage,
+  },
+  {
+    path: "/MapBuilding",
+    component: MapBuilding,
+  },
+];
+
+export default function RouteConfigExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/tacos">Tacos</Link>
+          </li>
+          <li>
+            <Link to="/sandwiches">Sandwiches</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          {routes.map((route, i) => (
+            <RouteWithSubRoutes key={i} {...route} />
+          ))}
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+// A special wrapper for <Route> that knows how to
+// handle "sub"-routes by passing them in a `routes`
+// prop to the component it renders.
+function RouteWithSubRoutes(route) {
+  return (
+    <Route
+      path={route.path}
+      render={props => (
+        // pass the sub-routes down to keep nesting
+        <route.component {...props} routes={route.routes} />
+      )}
+    />
+  );
+}
