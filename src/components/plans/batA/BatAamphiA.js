973926837e5f7628b9f2cf90e8a11933b76fec32
nbinet <nathan.binet99@gmail.com>
2021-07-22 17:01:49 +0200

    Header and footer add css + add amphi

--- src/components/plans/batA/BatAamphiA.js
+++ src/components/plans/batA/BatAamphiA.js
@@ -1,12 +1,27 @@
 import { MapInteractionCSS } from "react-map-interaction";
-import plop from "../../../assets/plansPNG/batCamphA.png";
+import plansvg from "../../../assets/plansPNG/plan.svg";
+import bigheart from "../../../assets/icones/bigheart.svg";
+import details from "../../../assets/icones/details.svg";
+import "./BatAamphiA.scss";
 
 export const batAamphiA = () => {
   return (
-    <div style={{ height: "120%" }}>
-      <MapInteractionCSS>
-        <img src={plop} />
+    <div className="pageAmphi">
+      <p className="textAmphi">Amphi. A</p>
+      <p className="textBat">Bat. A</p>
+      <MapInteractionCSS className="amphi">
+        <div className="amphi">
+          <img alt="plansvg" src={plansvg} />
+        </div>
       </MapInteractionCSS>
+      <div className="dflex">
+        <img alt="bigHeart" className="img" src={bigheart} />
+        <p>Mettre en favoris</p>
+      </div>
+      <div className="dflex">
+        <img alt="details" className="img" src={details} />
+        <p>Afficher la légende</p>
+      </div>
     </div>
   );
 };
